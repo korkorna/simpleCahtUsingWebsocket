f5367bdd8b50c01d16dd1ba3e61d2d7e9ba17e2d
korkorna <korkorna@gmail.com>
2016-11-06 12:30:13 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Simple Chat Using Websocket</title>
+</head>
+
+<body>
+	<div class="chat_wrapper">
+		<div class="message_box" id="message_box"></div>
+
+		<div class="panel">
+			<input type="text" name="name" id="name" placeholder="Your Name" maxlength="25">
+			<input type="text" name="message" id="message" placeholder="Message" maxlength="255">
+			<button type="button" id="send-btn">Send</button>
+		</div>
+	</div>
+
+</body>
+
+</html>
